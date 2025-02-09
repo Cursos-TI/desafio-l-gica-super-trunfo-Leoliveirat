7acf539b26f05d56da65a7e28b89fa012fa43642
Leoliveirat <Leo.oliveira2184@gmail.com>
2025-02-09 13:35:11 +0000

    Versão 6

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -7,15 +7,15 @@ int main() {
     char pais1[30], pais2[30];
     int populacao1, pontosturisticos1, populacao2, pontosturisticos2;
     float area1, pib1, area2, pib2, pibpercapta1, pibpercapta2, densidade1, densidade2, superpoder1,superpoder2;
-    int opcao, opcao2, opcao3;
+    int opcao, opcao2, opcao3, opcao4;
     char* resultado;
 
     printf ("*#*# BEM VINDO AO SUPER TRUNFO #*#* \n");
     printf ("\n");
     printf ("O Super Trunfo é um jogo no qual os jogadores comparam as propriedades de suas cartas\n"); 
-    printf ("para determinar quem tem a mais forte. Cada carta\n"); 
-    printf ("possui várias propriedades. Vence a carta com o maior valor. Você poderá comparar\n");
-    printf ("somente 1 atributo entre as cartas para fazer a disputa, ou 2 atributos\n"); 
+    printf ("para determinar quem tem a carta mais forte. Cada carta\n"); 
+    printf ("possui várias propriedades. Vence a carta com o maior valor. Você poderá comparar,\n");
+    printf ("1 atributo ou 2 atributos entre as cartas para fazer a disputa, \n"); 
     printf ("\n");
     printf ("VAMOS JOGAR?\n");
     printf ("\n");
@@ -26,7 +26,6 @@ int main() {
 //CADASTRO DAS CARTAS
 
     //CARTA 1
-
 printf ("CADASTRE A CARTA 1 \n");
 
 printf("Digite o País: \n");
@@ -55,11 +54,9 @@ printf("\n");
 
 superpoder1 = (pontosturisticos1 + area1 + pib1 + densidade1 + pibpercapta1);
 printf("Super poder: %f \n", superpoder1);
-
 printf("\n");
 
 //CARTA 2
-
 printf ("CADASTRE A CARTA 2 \n");
 
 printf("Digite o País: \n");
@@ -88,21 +85,16 @@ printf("\n");
 
 superpoder2 = (pontosturisticos2 + area2 + pib2 + densidade2 + pibpercapta2);
 printf("Super poder: %f \n", superpoder2);
-
 printf("\n");
 
 
-
 //MENU DE ESCOLHA DO MODO DE COMPARAÇÃO
-
   printf("Deseja escolher um ou dois atributos? Escolha a opção digitando 1 ou 2\n");
   printf("1 - Um atributo \n");
   printf("2 - Dois atributos\n");
   scanf("%d", &opcao);
 
 //MENU DE ESCOLHA DOS ATRIBUTOS
-  
-
 switch (opcao) {
 case 1:
 printf("Escolha o atributo que você deseja comparar, Escolha digitando o número da opção.\n");
@@ -117,7 +109,7 @@ printf("\n");
 printf("Digite o número do atributo: \n");
 scanf("%d", &opcao2);
 
-        // Agora AS COMPARAÇÕES
+         //CÁLCULOS PARA AS COMPARAÇÕES E DEFINIÇÃO DA VENCEDORA
          if (opcao2 == 1) {
             resultado = (populacao1 > populacao2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
             printf("Resultado: %s\n", resultado);
@@ -144,9 +136,7 @@ scanf("%d", &opcao2);
         }
         break;
           
-          
-          //MENU DE ESCOLHA DE 2 ATRIBUTOS
-case 2: 
+case 2: //MENU DE ESCOLHA DOS 2 ATRIBUTOS
 printf("Escolha os dois atributos que você deseja comparar, Escolha digitando o número da opção.\n");
 printf("1 - População\n");
 printf("2 - Área\n");
@@ -157,68 +147,100 @@ printf("6 - PIB Per capta\n");
 printf("7 - Super poder\n");
 printf("\n");
 
+printf("Digite o número do primeiro atributo: \n");
+scanf("%d", &opcao3);
+printf("Digite o número do segundo atributo: \n");
+scanf("%d", &opcao4);
 
-          //****PROFESSOR, ATÉ AQUI EU FIZ SOZINHO E POR CONTA PRÓPRIA 
-          // MAS, CONFESSO QUE A PARTIR DAQUI, EU TIVE UMA DIFICULDADE MAIOR 
-          //DEI UMA TRAVADA E ACABEI UTILIZANDO PELA PRIMEIRA VEZ A IA PARA ME AUXILIAR
-          //PELO MENOS EM COMO MONTAR ESSA LOGICA, POIS EU ESTAVA PENSANDO EM FAZER
-          //ATRAVES DE SWITCH, MAS NAO ESTAVA CONSEGUINDO CHEGAR A UMA LOGICA
 
-          // Perguntar para o usuário escolher os dois atributos
-          printf("Digite o número do primeiro atributo: \n");
-          scanf("%d", &opcao2);
-          printf("Digite o número do segundo atributo: \n");
-          scanf("%d", &opcao3);
+        //COMPARAÇÕES DE 2 ATRIBUTOS E CÁLCULOS PARA A DEFINIÇÃO DA VENCEDORA
+        if (opcao3 == 1 && opcao4 == 2) { 
+            resultado = (populacao1 > populacao2 && area1 > area2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
+            printf("Resultado: %s\n", resultado);
+        } 
+        else if (opcao3 == 1 && opcao4 == 3) {
+            resultado = (populacao1 > populacao2 && pib1 > pib2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
+            printf("Resultado: %s\n", resultado);
+        } 
+        else if (opcao3 == 1 && opcao4 == 4) {
+            resultado = (populacao1 > populacao2 && pontosturisticos1 > pontosturisticos2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
+            printf("Resultado: %s\n", resultado);
+        } 
+        else if (opcao3 == 1 && opcao4 == 5) {
+            resultado = (populacao1 > populacao2 && densidade1 > densidade2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
+            printf("Resultado: %s\n", resultado);
+        } 
+        else if (opcao3 == 1 && opcao4 == 6) {
+            resultado = (populacao1 > populacao2 && pibpercapta1 > pibpercapta2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
+            printf("Resultado: %s\n", resultado);
+        } 
+        else if (opcao3 == 1 && opcao4 == 7) {
+            resultado = (populacao1 > populacao2 && superpoder1 > superpoder2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
+            printf("Resultado: %s\n", resultado);
+        }
+        else if (opcao3 == 2 && opcao4 == 3) { 
+            resultado = (area1 > area2 && pib1 > pib2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
+            printf("Resultado: %s\n", resultado);
+        } 
+        else if (opcao3 == 2 && opcao4 == 4) { 
+            resultado = (area1 > area2 && pontosturisticos1 > pontosturisticos2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
+            printf("Resultado: %s\n", resultado);
+        } 
+        else if (opcao3 == 2 && opcao4 == 5) { 
+            resultado = (area1 > area2 && densidade1 > densidade2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
+            printf("Resultado: %s\n", resultado);
+        } 
+        else if (opcao3 == 2 && opcao4 == 6) { 
+            resultado = (area1 > area2 && pibpercapta1 > pibpercapta2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
+            printf("Resultado: %s\n", resultado);
+        } 
+        else if (opcao3 == 2 && opcao4 == 7) { 
+            resultado = (area1 > area2 && superpoder1 > superpoder2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
+            printf("Resultado: %s\n", resultado);
+        }
+        else if (opcao3 == 3 && opcao4 == 4) {
+            resultado = (pib1 > pib2 && pontosturisticos1 > pontosturisticos2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
+            printf("Resultado: %s\n", resultado);
+        } 
+        else if (opcao3 == 3 && opcao4 == 5) {
+            resultado = (pib1 > pib2 && densidade1 > densidade2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
+            printf("Resultado: %s\n", resultado);
+        } 
+        else if (opcao3 == 3 && opcao4 == 6) {
+            resultado = (pib1 > pib2 && pibpercapta1 > pibpercapta2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
+            printf("Resultado: %s\n", resultado);
+        } 
+        else if (opcao3 == 3 && opcao4 == 7) {
+            resultado = (pib1 > pib2 && superpoder1 > superpoder2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
+            printf("Resultado: %s\n", resultado);
+        }
+        else if (opcao3 == 4 && opcao4 == 5) {
+            resultado = (pontosturisticos1 > pontosturisticos2 && densidade1 > densidade2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
+            printf("Resultado: %s\n", resultado);
+        } 
+        else if (opcao3 == 4 && opcao4 == 6) {
+            resultado = (pontosturisticos1 > pontosturisticos2 && pibpercapta1 > pibpercapta2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
+            printf("Resultado: %s\n", resultado);
+        } 
+        else if (opcao3 == 4 && opcao4 == 7) {
+            resultado = (pontosturisticos1 > pontosturisticos2 && superpoder1 > superpoder2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
+            printf("Resultado: %s\n", resultado);
+        }
+        else if (opcao3 == 5 && opcao4 == 6) {
+            resultado = (densidade1 > densidade2 && pibpercapta1 > pibpercapta2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
+            printf("Resultado: %s\n", resultado);
+        } 
+        else if (opcao3 == 5 && opcao4 == 7) {
+            resultado = (densidade1 > densidade2 && superpoder1 > superpoder2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
+            printf("Resultado: %s\n", resultado);
+        }
+        else if (opcao3 == 6 && opcao4 == 7) {
+            resultado = (pibpercapta1 > pibpercapta2 && superpoder1 > superpoder2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
+            printf("Resultado: %s\n", resultado);
+        }
+        break;
 
-          // Comparar os dois atributos escolhidos
-          if (opcao2 == 1 && opcao3 == 2) { // População e Área
-              resultado = (populacao1 > populacao2 && area1 > area2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
-              printf("Resultado: %s\n", resultado);
-          } else if (opcao2 == 1 && opcao3 == 3) { // População e PIB
-              resultado = (populacao1 > populacao2 && pib1 > pib2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
-              printf("Resultado: %s\n", resultado);
-          } else if (opcao2 == 1 && opcao3 == 4) { // População e Pontos Turísticos
-              resultado = (populacao1 > populacao2 && pontosturisticos1 > pontosturisticos2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
-              printf("Resultado: %s\n", resultado);
-          } else if (opcao2 == 1 && opcao3 == 5) { // População e Densidade
-              resultado = (populacao1 > populacao2 && densidade1 > densidade2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
-              printf("Resultado: %s\n", resultado);
-          } else if (opcao2 == 1 && opcao3 == 6) { // População e PIB per capta
-              resultado = (populacao1 > populacao2 && pibpercapta1 > pibpercapta2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
-              printf("Resultado: %s\n", resultado);
-          } else if (opcao2 == 1 && opcao3 == 7) { // População e Super poder
-              resultado = (populacao1 > populacao2 && superpoder1 > superpoder2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
-              printf("Resultado: %s\n", resultado);
-          } else if (opcao2 == 2 && opcao3 == 3) { // Área e PIB
-              resultado = (area1 > area2 && pib1 > pib2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
-              printf("Resultado: %s\n", resultado);
-          } else if (opcao2 == 2 && opcao3 == 4) { // Área e Pontos Turísticos
-              resultado = (area1 > area2 && pontosturisticos1 > pontosturisticos2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
-              printf("Resultado: %s\n", resultado);
-          } else if (opcao2 == 2 && opcao3 == 5) { // Área e Densidade
-              resultado = (area1 > area2 && densidade1 > densidade2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
-              printf("Resultado: %s\n", resultado);
-          } else if (opcao2 == 2 && opcao3 == 6) { // Área e PIB per capta
-              resultado = (area1 > area2 && pibpercapta1 > pibpercapta2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
-              printf("Resultado: %s\n", resultado);
-          } else if (opcao2 == 2 && opcao3 == 7) { // Área e Super poder
-              resultado = (area1 > area2 && superpoder1 > superpoder2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
-              printf("Resultado: %s\n", resultado);
-          } else if (opcao2 == 3 && opcao3 == 4) { // PIB e Pontos Turísticos
-              resultado = (pib1 > pib2 && pontosturisticos1 > pontosturisticos2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
-              printf("Resultado: %s\n", resultado);
-          } else if (opcao2 == 3 && opcao3 == 5) { // PIB e Densidade
-              resultado = (pib1 > pib2 && densidade1 > densidade2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
-              printf("Resultado: %s\n", resultado);
-          } else if (opcao2 == 3 && opcao3 == 6) { // PIB e PIB per capta
-              resultado = (pib1 > pib2 && pibpercapta1 > pibpercapta2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
-              printf("Resultado: %s\n", resultado);
-          } else if (opcao2 == 3 && opcao3 == 7) { // PIB e Super poder
-              resultado = (pib1 > pib2 && superpoder1 > superpoder2) ? "A CARTA 1 É A VENCEDORA!" : "A CARTA 2 É A VENCEDORA!";
-              printf("Resultado: %s\n", resultado);
-          }
-          break;
-  }        
+}       
 
 
 
